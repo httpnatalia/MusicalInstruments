754b2da88f2fda25daca4f88d483cf7004c7091d
Natalia <oliver_na@hotmail.com.br>
2022-11-22 21:34:27 -0300

    function tocaSom

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio){
+  const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
  
 const listaDeTeclas = document.querySelectorAll('.tecla');
